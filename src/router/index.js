f57ff00e9348860ebaddc2af70210351cc953d52
choisungwook <kgg1959@naver.com>
2021-09-19 01:45:27 +0900

    feat: helloworld 페이지 추가 #1

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Helloworld from "../views/Helloworld.vue";
 
 Vue.use(VueRouter);
 
@@ -8,16 +8,16 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home,
+    component: Helloworld,
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/helloworld",
+    name: "Hellowlrd",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "about" */ "../views/Helloworld.vue"),
   },
 ];
 
---
+++ src/views/Helloworld.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>This is helloworld page.</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Helloworld",
+};
+</script>
