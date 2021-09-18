ae1fa03a4ef46eaf3a2d49bf6eb8d60d170bf403
choisungwook <kgg1959@naver.com>
2021-09-19 02:16:35 +0900

    사이드 메뉴 네비게이션 #2

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,28 @@
 <template>
   <v-app>
+    <v-navigation-drawer app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6"> Application </v-list-item-title>
+          <v-list-item-subtitle> subtext </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link :to="item.to">
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-main>
       <router-view />
     </v-main>
@@ -10,8 +33,14 @@
 export default {
   name: "App",
 
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      items: [
+        { title: "home", icon: "mdi-view-dashboard", to: "/" },
+        { title: "helloworld", icon: "mdi-image", to: "/helloworld" },
+      ],
+      right: null,
+    };
+  },
 };
 </script>
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>This is Home.</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
