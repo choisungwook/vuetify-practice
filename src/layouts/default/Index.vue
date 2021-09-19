e7f91e77b71378d4b80719f3c6ad7c62763214cc
choisungwook <kgg1959@naver.com>
2021-09-19 16:59:47 +0900

    vuex를 이용해서 메뉴 데이터 이동 #8

--- src/layouts/default/Index.vue
+++ src/layouts/default/Index.vue
@@ -17,13 +17,13 @@
       <v-divider></v-divider>
 
       <v-list dense nav>
-        <v-list-item v-for="item in items" :key="item.title" link :to="item.to">
+        <v-list-item v-for="menu in menus" :key="menu.title" link :to="menu.to">
           <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
+            <v-icon>{{ menu.icon }}</v-icon>
           </v-list-item-icon>
 
           <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
+            <v-list-item-title>{{ menu.title }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -38,6 +38,8 @@
 </template>
 
 <script>
+import { mapState } from "vuex";
+
 export default {
   name: "DefaultLayout",
 
@@ -47,7 +49,18 @@ export default {
       { title: "helloworld", icon: "mdi-image", to: "/helloworld" },
     ],
     right: null,
-    drawer: false,
   }),
+
+  computed: {
+    ...mapState("menu", ["menus"]),
+    drawer: {
+      get() {
+        return this.$store.getters["menu/getDrawer"];
+      },
+      set(value) {
+        return this.$store.dispatch("menu/toggleDrawer", value);
+      },
+    },
+  },
 };
 </script>
---
+++ src/store/modules/menu.js
@@ -0,0 +1,31 @@
+const state = {
+  menus: [
+    { title: "home", icon: "mdi-view-dashboard", to: "/" },
+    { title: "helloworld", icon: "mdi-image", to: "/helloworld" },
+  ],
+  drawer: false,
+};
+
+const getters = {
+  getDrawer: (state) => state.drawer,
+};
+
+const mutations = {
+  setDrawer: (state, data) => {
+    state.drawer = data;
+  },
+};
+
+const actions = {
+  toggleDrawer({ commit }, value) {
+    commit("setDrawer", value);
+  },
+};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions,
+};
